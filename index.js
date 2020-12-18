e1df353bd01d8f6a139334451711ae7f14b78dc1
Ben McRae <benmcrae5@yahoo.com>
2020-12-17 22:08:55 -0700

    starting files and setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2/promise');
+const cTable = require('console.table');
+
+
+//create main menu
+//create view all departments function
+//create view all roles function
+//create view all employees function
+//create add functions for : department, role, employee
+//create an update employee role function
